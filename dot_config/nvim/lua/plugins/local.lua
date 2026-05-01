270d8f4e484600a6d65613cba860de212ec427a2
Noah Moss <noahbmoss@gmail.com>
2026-04-30 20:58:18 -0400

    Update local.lua

--- dot_config/nvim/lua/plugins/local.lua
+++ dot_config/nvim/lua/plugins/local.lua
@@ -1,6 +1,12 @@
+local docent_dir = vim.fn.expand("~/Projects/docent/nvim")
+
+if vim.fn.isdirectory(docent_dir) == 0 then
+	return {}
+end
+
 return {
 	{
-		dir = "~/Projects/docent/nvim",
+		dir = docent_dir,
 		config = function()
 			require("docent").setup()
 		end,
