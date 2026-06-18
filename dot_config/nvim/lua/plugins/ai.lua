6985ee8060f63fbeb766828e076836b2ce1bd495
Noah Moss <noahbmoss@gmail.com>
2026-06-18 16:02:35 -0400

    Update ai.lua Update .shellrc

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -1,9 +1,18 @@
 return {
 	{
-		"greggh/claude-code.nvim",
+		"coder/claudecode.nvim",
 		dependencies = {
-			"nvim-lua/plenary.nvim",
+			"folke/snacks.nvim",
 		},
-		config = true,
+		opts = {
+			snacks_win_opts = {
+				keys = {
+					nav_left  = { "<C-h>", function() vim.cmd("TmuxNavigateLeft")  end, mode = "t", desc = "Navigate left" },
+					nav_down  = { "<C-j>", function() vim.cmd("TmuxNavigateDown")  end, mode = "t", desc = "Navigate down" },
+					nav_up    = { "<C-k>", function() vim.cmd("TmuxNavigateUp")    end, mode = "t", desc = "Navigate up" },
+					nav_right = { "<C-l>", function() vim.cmd("TmuxNavigateRight") end, mode = "t", desc = "Navigate right" },
+				},
+			},
+		},
 	},
 }
