224d354721c1e38406339a75256326c109769163
Noah Moss <noahbmoss@gmail.com>
2026-06-18 16:10:41 -0400

    Update ai.lua

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -4,6 +4,10 @@ return {
 		dependencies = {
 			"folke/snacks.nvim",
 		},
+		keys = {
+			{ "<leader>a",  "<cmd>ClaudeCode<cr>",     desc = "[A]I toggle" },
+			{ "<leader>as", "<cmd>ClaudeCodeSend<cr>", desc = "[A]I [S]end", mode = { "n", "v" } },
+		},
 		config = true,
 	},
 }
