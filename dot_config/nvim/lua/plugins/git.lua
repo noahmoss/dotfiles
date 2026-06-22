79d155728964ec02dd47408d55b3e813a993c946
Noah Moss <noahbmoss@gmail.com>
2026-06-21 23:21:01 -0400

    Update git.lua

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,5 +1,39 @@
 return {
-	"tpope/vim-fugitive",
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "fugitiveblame",
+				callback = function(ev)
+					vim.keymap.set("n", "gx", function()
+						local hash = vim.api.nvim_get_current_line():match("^%s*(%x+)")
+						if not hash or hash:match("^0+$") then
+							return
+						end
+						local remote = vim.fn.system("git remote get-url origin 2>/dev/null"):gsub("\n", "")
+						local owner, repo = remote:match("github%.com[:/]([^/]+)/([^/\n%.]+)")
+						if not owner then
+							vim.notify("Could not determine GitHub repo", vim.log.levels.ERROR)
+							return
+						end
+						local url = vim.fn
+							.system(string.format(
+								"gh api repos/%s/%s/commits/%s/pulls --jq '.[0].html_url // empty' 2>/dev/null",
+								owner,
+								repo,
+								hash
+							))
+							:gsub("\n", "")
+						if url == "" then
+							vim.notify("No PR found for " .. hash, vim.log.levels.WARN)
+							return
+						end
+						vim.fn.system("open " .. vim.fn.shellescape(url))
+					end, { buffer = ev.buf, desc = "Open GitHub PR for blame commit" })
+				end,
+			})
+		end,
+	},
 	"tpope/vim-rhubarb",
 	"sindrets/diffview.nvim",
 
